e12a8fb9219b942804651aad0d9f93951d7808b8
joaocarrato <joaocarrato18@gmail.com>
2023-12-05 20:39:49 -0300

    #

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
 import Toast from 'react-native-toast-message';
 import Router from './src/routes/router';
 import { colors } from './src/shared/themes/themes';
@@ -8,6 +8,7 @@ import { colors } from './src/shared/themes/themes';
 const App = () => {
   return (
     <NavigationContainer>
+      <StatusBar backgroundColor={colors.background} />
       <SafeAreaView style={styles.container}>
         <Router />
       </SafeAreaView>
